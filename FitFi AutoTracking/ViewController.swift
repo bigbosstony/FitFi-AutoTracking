110921e286d71567ac235d99bda198e35f937331
ton1 <macintosh711@gmail.com>
2019-01-07 17:23:11 -0500

    Initial Commit

---
+++ FitFi AutoTracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitFi AutoTracking
+//
+//  Created by YAN YU on 2019-01-07.
+//  Copyright © 2019 YAN YU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FitFi AutoTrackingTests/FitFi_AutoTrackingTests.swift
@@ -0,0 +1,34 @@
+//
+//  FitFi_AutoTrackingTests.swift
+//  FitFi AutoTrackingTests
+//
+//  Created by YAN YU on 2019-01-07.
+//  Copyright © 2019 YAN YU. All rights reserved.
+//
+
+import XCTest
+@testable import FitFi_AutoTracking
+
+class FitFi_AutoTrackingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
